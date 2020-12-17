3451f988d878db14b6579dbcaf21badfe6a0e92b
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2020-12-16 22:12:26 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#while True:
-    #print("light level:"+ input.light_level())
-    #if input.light_level() < 7: 
-        #light.set_all(light.rgb(255, 255, 255))
-    #else:
-        #light.clear()
-
 while True:
     print("light level:"+ input.light_level())
-    if input.light_level() < 6: 
-        light.set_all(light.rgb(0, 0, 255))
-    elif input.light_level() > 13:
-        light.set_all(light.rgb(255, 255, 0))
+    if input.light_level() < 7: 
+        light.set_all(light.rgb(255, 255, 255))
     else:
         light.clear()
+
+#while True:
+    #print("light level:"+ input.light_level())
+    #if input.light_level() < 5: 
+        #light.set_all(light.rgb(0, 0, 255))
+    #elif input.light_level() > 13:
+        #light.set_all(light.rgb(255, 255, 0))
+    #else:
+        #light.clear()
--- main.ts
+++ main.ts
@@ -1,15 +1,7 @@
-// while True:
-// print("light level:"+ input.light_level())
-// if input.light_level() < 7: 
-// light.set_all(light.rgb(255, 255, 255))
-// else:
-// light.clear()
 while (true) {
     console.log("light level:" + input.lightLevel())
-    if (input.lightLevel() < 6) {
-        light.setAll(light.rgb(0, 0, 255))
-    } else if (input.lightLevel() > 13) {
-        light.setAll(light.rgb(255, 255, 0))
+    if (input.lightLevel() < 7) {
+        light.setAll(light.rgb(255, 255, 255))
     } else {
         light.clear()
     }
