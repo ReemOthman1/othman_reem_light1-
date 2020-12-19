290c38fc245a161324a46b0c448e123db4eb8fea
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2020-12-19 00:23:07 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ while True:
     if input.light_level() < 6: 
         light.set_all(light.rgb(0, 0, 255))
     elif input.light_level() > 13:
-        light.set_all(light.rgb(255, 255, 0))
+        light.set_all(light.rgb(255,165,0))
     else:
         light.clear()
